b69c7a5634ab4cde74132f03c3b58b2a7a639c73
tmvkdpf86 <tmvkdpf86@gmail.com>
2026-08-13 00:28:48 +0900

    로또번호 생성기능추가

--- main.py
+++ main.py
@@ -4,3 +4,14 @@ st.title("Hello Streamlit")
 st.write('안녕하세요. 배포 참 쉽죠?')
 st.write(time.strftime('%Y-%m-%d %H:%m:%s'))
 
+st.title("sparkles: 로또생성기 :sparkles:")
+def generate_lotto():
+  lotto = [i + 1 for i in range(45)]
+  random.shuffle(lotto)
+  return lotto[:6]
+
+botton = st.button["로또를 생성해 주세요"]
+
+if button:
+  for i in range(5):
+    st.subheader(f"{1+1}행운의번호: :green[{generate_lotto()}]")
